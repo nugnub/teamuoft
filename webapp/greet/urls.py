7d713d940b69c4ae10de8781d398a4060bac4f69
nugnub <assad.illya@gmail.com>
2020-06-04 17:17:35 -0400

    illyas branch

--- webapp/greet/urls.py
+++ webapp/greet/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = {
     path('', views.hello, name='hello'),
     path('nvm', views.bye, name = 'bye')
+    
+
 }
--- webapp/greet/views.py
+++ webapp/greet/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("hello world!")
+    return HttpResponse("hello world!!!!!!!!")
 
 def bye(request):
-    return HttpResponse("good bye!")
+    return HttpResponse("good bye!!!!!!!!!!!!!!!!!!!")
